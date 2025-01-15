62e1b5da0fa53f1e136f0dfa34524c6dd65e96e3
evelynforkhands <lyubaevadaria@gmail.com>
2025-01-15 18:14:15 +0100

    chore: formatting

--- lib/snowplow/tracker.ts
+++ lib/snowplow/tracker.ts
@@ -1,15 +1,17 @@
-import { newTracker } from '@snowplow/node-tracker';
+import { newTracker } from "@snowplow/node-tracker";
 
-const tracker = newTracker({
-    namespace: "blog-tracker", 
-    appId: "blog",
-    encodeBase64: false, 
-  }, {
-    endpoint: "http://localhost", 
-    port: 9090, // for local dev - to talk to Snowplow Micro
-    eventMethod: "post", 
-    bufferSize: 1, // only send events once n are buffered
-  });
-
+const tracker = newTracker(
+    {
+        namespace: "blog-tracker",
+        appId: "blog",
+        encodeBase64: false,
+    },
+    {
+        endpoint: "http://localhost",
+        port: 9090, // for local dev - to talk to Snowplow Micro
+        eventMethod: "post",
+        bufferSize: 1, // only send events once n are buffered
+    }
+);
 
 export default tracker;
