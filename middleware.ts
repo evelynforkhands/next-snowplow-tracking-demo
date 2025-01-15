01cf293e1a425327303471d25b8447f568684ab4
evelynforkhands <lyubaevadaria@gmail.com>
2025-01-15 18:22:18 +0100

    wip

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,7 @@
 import trackPageView from "lib/snowplow/trackPageView";
-import { setUserProperties } from "lib/snowplow/utils/userContext";
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-    setUserProperties({
-        userAgent: request.headers.get("user-agent") || undefined,
-    });
     trackPageView({
         pageUrl: request.url,
         referrer: request.headers.get("referer") || undefined,
